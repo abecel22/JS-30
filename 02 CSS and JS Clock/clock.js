8821bb6c45b577f3b2f331bdbd7a6f7cdced3899
abecel22 <abe2211@gmail.com>
2017-09-23 20:06:29 -0500

    Finished JS on clock. Min, sec, and hr.

---
+++ 02 CSS and JS Clock/clock.js
@@ -0,0 +1,25 @@
+
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+    //sets the seconds rotations
+    const seconds = now.getSeconds();
+    const secondDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+    //sets the minutes rotations
+    const min = now.getMinutes();
+    const minDegrees = ((min / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minDegrees}deg)`;
+
+    //sets the hour rotations
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+
+}
+
+setInterval(setDate, 1000);
