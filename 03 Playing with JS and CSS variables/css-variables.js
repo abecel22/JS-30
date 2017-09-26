9b1a0b22e2f37085116eadfac43dec1f8f800ba3
abecel22 <abe2211@gmail.com>
2017-09-25 20:54:47 -0500

    Finalized JS to update variables based on input.

--- 03 Playing with JS and CSS variables/css-variables.js
+++ 03 Playing with JS and CSS variables/css-variables.js
@@ -2,8 +2,10 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
-    console.log(this.value);
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
+//event listeners are added and then they call for handleUpdate function
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));    
