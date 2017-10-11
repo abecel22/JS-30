899ed3b91390073a744b811b0c12dd58c581a117
abecel22 <abe2211@gmail.com>
2017-10-10 20:53:48 -0500

    Added play and pause functionality and icons to JS.

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,30 @@
+// Get our Elements
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const prgressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
+
+// Build our functions
+
+function togglePlay() {
+   const method = video.paused ? 'play' : 'pause';
+   video[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+
+
+// Hook up event listeners
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
